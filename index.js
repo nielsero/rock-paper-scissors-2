e4d0cf0b151c0eb177e2fbdf0272d92304d27c25
Niels <niels.cpp@gmail.com>
2022-04-15 22:26:21 +0200

    Implemented game function
    Used a for loop to make it play 5 rounds.

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const CHOICES = ["rock", "paper", "scissors"];
 const RULES = [["rock", "scissors"], ["paper", "rock"], ["scissors", "paper"]];
 
 // Makes the computer choose rock, paper or scissors at random
-const computerPlay = (choices) => {
-    const randomNumber = Math.floor(Math.random() * choices.length);
-    return choices[randomNumber];
+const computerPlay = () => {
+    const randomNumber = Math.floor(Math.random() * CHOICES.length);
+    return CHOICES[randomNumber];
 }
 
 // To check if player won
-const hasPlayerWon = (playerSelection, computerSelection, rules) => {
-    for(const rule of rules) {
+const hasPlayerWon = (playerSelection, computerSelection) => {
+    for(const rule of RULES) {
         if(rule[0] === playerSelection && rule[1] === computerSelection) {
             return true;
         }
@@ -21,20 +21,31 @@ const hasPlayerWon = (playerSelection, computerSelection, rules) => {
 const clean = (playerSelection) => (playerSelection.trim().toLowerCase());
 
 // To play a single round
-const playRound = (playerSelection, computerSelection, choices) => {
+const playRound = (playerSelection, computerSelection) => {
     playerSelection = clean(playerSelection);
-    if(!choices.includes(playerSelection)) {
+    if(!CHOICES.includes(playerSelection)) {
         return "Invalid choice";
     }
     if(playerSelection == computerSelection) {
         return `It's a draw, you both picked ${playerSelection}`;
     }
-    if(hasPlayerWon(playerSelection, computerSelection, RULES)) {
+    if(hasPlayerWon(playerSelection, computerSelection)) {
         return `You won, ${playerSelection} beats ${computerSelection}`;
     }
     return `You lose, ${computerSelection} beats ${playerSelection}`;
 }
 
-const computerSelection = computerPlay(CHOICES);
-console.log("Computer: " + computerSelection);
-console.log(playRound(" rOck", computerSelection, CHOICES));
+// To play 5 rounds
+const game = () => {
+    let playerSelection;
+    for(let i = 0; i < 5; i++) {
+        playerSelection = prompt("Rock, Paper or Scissors?");
+        computerSelection = computerPlay();
+        console.log(`You: ${clean(playerSelection)}`);
+        console.log(`Computer: ${computerSelection}`);
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("------------------------------------------");
+    }
+}
+
+game();
