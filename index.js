fec11f3c36a586d146e1ff4a92603b9692459716
Niels <niels.cpp@gmail.com>
2022-04-15 22:10:55 +0200

    Implemented play round
    Created a function to check if player won (hasPlayerWon).
    Another one to clean player input (clean).
    The last one to play the round (playRound).
    Also added RULES and CHOICES arrays, to check winner and validate choices.

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
+const CHOICES = ["rock", "paper", "scissors"];
+const RULES = [["rock", "scissors"], ["paper", "rock"], ["scissors", "paper"]];
+
 // Makes the computer choose rock, paper or scissors at random
-const computerPlay = () => {
-    const choices = ["rock", "paper", "scissors"];
+const computerPlay = (choices) => {
     const randomNumber = Math.floor(Math.random() * choices.length);
     return choices[randomNumber];
 }
+
+// To check if player won
+const hasPlayerWon = (playerSelection, computerSelection, rules) => {
+    for(const rule of rules) {
+        if(rule[0] === playerSelection && rule[1] === computerSelection) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// To clean player selection
+const clean = (playerSelection) => (playerSelection.trim().toLowerCase());
+
+// To play a single round
+const playRound = (playerSelection, computerSelection, choices) => {
+    playerSelection = clean(playerSelection);
+    if(!choices.includes(playerSelection)) {
+        return "Invalid choice";
+    }
+    if(playerSelection == computerSelection) {
+        return `It's a draw, you both picked ${playerSelection}`;
+    }
+    if(hasPlayerWon(playerSelection, computerSelection, RULES)) {
+        return `You won, ${playerSelection} beats ${computerSelection}`;
+    }
+    return `You lose, ${computerSelection} beats ${playerSelection}`;
+}
+
+const computerSelection = computerPlay(CHOICES);
+console.log("Computer: " + computerSelection);
+console.log(playRound(" rOck", computerSelection, CHOICES));
